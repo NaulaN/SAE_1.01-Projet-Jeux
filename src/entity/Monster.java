8f31c68d6136a2413ff6a2c4948c229120f0b950
NaulaN <nonochrzaszcz@gmail.com>
2021-12-23 20:38:16 +0100

    Update system

--- src/entity/Monster.java
+++ src/entity/Monster.java
@@ -1,27 +1,28 @@
 package entity;
 
-
 import static entity.Const.*;
+
 
 public class Monster extends Entity
 {
     public Monster(int x, int y, int velocity) { super("Monster", x, y, velocity); }
 
     /**
-     * Fait bougé le monstre aléatoirement
+     * Bouge le monstre aléatoirement.
+     * @param maps Donnez la matrice de donnée de la Maps
      */
     public void randomMove(char[][] maps)
     {
         int moveRandomly = (int) (Math.random()*RIGHT+1);
-        boolean[] collision = checkCollision(maps); // {UP, DOWN, LEFT, RIGHT}
+        boolean[] collision = checkCollision(maps);     // {UP, DOWN, LEFT, RIGHT}
 
         if (moveRandomly == UP && !collision[0])
             moveUp();
-        else if (moveRandomly == DOWN && !collision[1])
+        if (moveRandomly == DOWN && !collision[1])
             moveDown();
-        else if (moveRandomly == LEFT && !collision[2])
+        if (moveRandomly == LEFT && !collision[2])
             moveLeft();
-        else if (moveRandomly == RIGHT && !collision[3])
+        if (moveRandomly == RIGHT && !collision[3])
             moveRight();
     }
 }
